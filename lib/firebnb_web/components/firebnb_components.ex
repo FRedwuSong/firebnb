652937bed78cd40f01157c3e21a0d56962276131
FRedwuSong <diadora1166@gmail.com>
2023-06-04 16:46:56 +0800

    Use inner_block instead of attr for displaying price

--- lib/firebnb_web/components/firebnb_components.ex
+++ lib/firebnb_web/components/firebnb_components.ex
@@ -4,7 +4,7 @@ defmodule FirebnbWeb.Components do
   def price(assigns) do
     ~H"""
     <div class="font-bold text-gray-700">
-      €<%= @amount %> <span class="font-normal">night</span>
+      €<%= render_slot(@inner_block) %> <span class="font-normal">night</span>
     </div>
     """
   end
@@ -12,7 +12,7 @@ defmodule FirebnbWeb.Components do
   def room_title(assigns) do
     ~H"""
     <h1 class="pt-3 font-bold text-gray-700 line-clamp-2">
-      <%= @text %>
+      <%= render_slot(@inner_block) %>
     </h1>
     """
   end
