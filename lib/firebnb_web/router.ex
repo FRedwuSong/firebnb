314e36a98bc1c3ee9846c3b945f3922e734e0183
FRedwuSong <diadora1166@gmail.com>
2023-06-25 15:55:12 +0800

    phx.gen.auth's setting with router and layout

--- lib/firebnb_web/router.ex
+++ lib/firebnb_web/router.ex
@@ -17,12 +17,6 @@ defmodule FirebnbWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", FirebnbWeb do
-    pipe_through :browser
-
-    live "/", HomeLive.Index
-  end
-
   # Other scopes may use custom stacks.
   # scope "/api", FirebnbWeb do
   #   pipe_through :api
@@ -66,6 +60,7 @@ defmodule FirebnbWeb.Router do
 
     live_session :require_authenticated_user,
       on_mount: [{FirebnbWeb.UserAuth, :ensure_authenticated}] do
+      live "/", HomeLive.Index
       live "/users/settings", UserSettingsLive, :edit
       live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
     end
